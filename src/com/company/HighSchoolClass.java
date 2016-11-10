687d556493c7fa6e852bc7d28fccd65cb717c0de
gbarth1 <grace.m.barth@gmail.com>
2016-11-10 14:30:22 -0500

    Initial commit

---
+++ src/com/company/HighSchoolClass.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Created by gb647 on 11/10/16.
+ */
+public class HighSchoolClass
+{
+    private Student [] students;
+    
+
+    public Student getValedictorian()
+    {
+        //to be implemented
+    }
+
+    public double getHonorPercentage()
+    {
+
+    }
+}
